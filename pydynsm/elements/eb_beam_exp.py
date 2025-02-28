4aaaf33ea15205f7dd38ab0e739df20cfdb19103
GREYZF <zfang9831@gmail.com>
2025-02-28 13:43:05 +0100

    Revert "some changes to clean the codes"
    This reverts commit 64410ca640878d572951c9a91141ace02ff63aac.

--- pydynsm/elements/eb_beam_exp.py
+++ pydynsm/elements/eb_beam_exp.py
@@ -195,6 +195,7 @@ class EulerBernoulliBeam(StructuralElement):
         V = self.shearforce(x, omega, C)
         # get bending moment
         M = self.moment(x, omega, C)
+        
         return [V, M]
     
     def LocalElementStresses(self,u_nodes_local,omega,num_points):
@@ -218,10 +219,10 @@ class EulerBernoulliBeam(StructuralElement):
         
         # get shear force
         tau = self.shearstress(x, omega, C)
-        # # get bending moment
-        # sigma_yy = self.bendingstress(x, omega, C)
+        # get bending moment
+        sigma_yy = self.bendingstress(x, omega, C)
         
-        return [tau]
+        return [tau, sigma_yy]
 
     def Coefficients(self, u_nodes_local, omega):
         """
@@ -375,6 +376,40 @@ class EulerBernoulliBeam(StructuralElement):
         
         mmt = -E*I*kappa
         return mmt
+    
+    # def bendingstress(self, x, omega, C=None, u_node_local=None):
+    #     """
+    #     Get the moment field of the EB beam.
+
+    #     Input:
+    #         x: array. Points along element
+    #         omega: array. Range of frequencies of analysis
+    #         C: array. Values of coefficients of general solution
+    #         u_node_local: local nodes
+    #     Ouput:
+    #         mmt: array. moment field of the given element
+    #     Note:
+    #         if C is not given, then calculate it based on u_node_local.
+    #     """
+    #     # Read all the variables
+    #     rho = self.rho
+    #     A = self.A
+    #     L = self.L
+    #     E = self.E
+    #     I = self.Ib
+    #     W = self.Wb
+    #     alpha_1, alpha_2, alpha_3, alpha_4 = self.ElementWaveNumbers(omega)
+        
+    #     if C is None:
+    #         C = self.Coefficients(u_node_local, omega)
+        
+    #     kappa = ((-1j*alpha_1)**2*C[0]*np.exp(-1j*alpha_1*x) +
+    #             (-1j*alpha_2)**2*C[1]*np.exp(-1j*alpha_2*x) +
+    #             (-1j*alpha_3)**2*C[2]*np.exp(-1j*alpha_3*x) +
+    #             (-1j*alpha_4)**2*C[3]*np.exp(-1j*alpha_4*x))
+        
+    #     bs = -E*I*kappa/W
+    #     return bs
     
     def shearforce(self, x, omega, C=None, u_node_local=None):
         """
